a90fe328553287e33691f52c4ac100eac43ba40a
chkathome <67086249+chkathome@users.noreply.github.com>
2025-05-07 11:55:46 +0200

    Adjustments for new PlatformIO version

--- buildroot/share/PlatformIO/scripts/WEEDOR73.py
+++ buildroot/share/PlatformIO/scripts/WEEDOR73.py
@@ -2,7 +2,7 @@ import os
 Import("env", "projenv")
 
 # Relocate firmware from 0x08000000 to 0x08008000
-for define in env['CPPDEFINES']:
+for define in env['CPPDEFINES'].copy():
     if define[0] == "VECT_TAB_ADDR":
         env['CPPDEFINES'].remove(define)
 env['CPPDEFINES'].append(("VECT_TAB_ADDR", "0x08008000"))
